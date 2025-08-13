e50988c70ebf1cdf6d176a3d3f7dd9a6269be0f8
veeramachineni likhitha <166300421+veeramachinenilikhitha10@users.noreply.github.com>
2025-08-13 22:31:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from docx import Document
 import pdfplumber
 
-# ------- Utility Functions -------
+
 def load_job_description(path):
     with open(path, 'r', encoding='utf-8') as f:
         return f.read()
